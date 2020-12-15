878b6000547731b3471c5cb53bae30fc51bc6162
Hon <dennis.mejen@gmail.com>
2020-12-15 23:03:58 +0700

    [Updated] Update Dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,46 +1,32 @@
-import React from "react";
-
-const styles = {
-  dropdown: { position: "relative", display: "inline-block" },
-  "& a:hover": {
-    backgroundColor: "#ddd",
-  },
-  dropbtn: {
-    backgroundColor: "#3498DB",
-    color: "white",
-    padding: "16px",
-    fontSize: "16px",
-    border: "none",
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: "#2980B9",
-    },
-    "&:focus": {
-      backgroundColor: "#2980B9",
-    },
-  },
-  "dropdown-content": {
-    display: "none",
-    position: "absolute",
-    backgroundColor: "#f1f1f1",
-    minWidth: "160px",
-    overflow: "auto",
-    boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)",
-    zIndex: 1,
-  },
-};
+import React, { useState } from "react";
+import "./Dropdown.css";
 
 const Dropdown = () => {
+  const [display, setDisplay] = useState(false);
+
+  const handleClick = () => {
+    setDisplay(!display);
+  };
+
   return (
-    <div style={styles.dropdown}>
-      <button onClick="myFunction()" style={styles.dropbtn}>
+    <div className="dropdown">
+      <button
+        onClick={handleClick}
+        onBlur={() => setDisplay(false)}
+        className="dropbtn"
+      >
         Dropdown
       </button>
-      <div style={styles["dropdown-content"]}>
+      <div
+        id="myDropdown"
+        className="dropdown-content"
+        style={{ display: display ? "" : "none" }}
+      >
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
       </div>
+      Dropdown
     </div>
   );
 };
